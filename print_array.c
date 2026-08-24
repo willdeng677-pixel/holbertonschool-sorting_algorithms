5a9ccb47171237c4ba676361f034f9d6c640fa8b
willdeng677-pixel <willdeng677@gmail.com>
2026-08-24 00:51:54 +0000

    Function that print array

---
+++ print_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * print_array - prints an array of integers
+ * @array: array to be printed
+ * @size: size of the array
+ */
+void print_array(const int *array, size_t size)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+	}
+	printf("\n");
+}
