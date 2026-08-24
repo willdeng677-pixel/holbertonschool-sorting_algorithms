c260856f1e6beefc69b99cd750702054e7e99996
willdeng677-pixel <willdeng677@gmail.com>
2026-08-24 01:00:09 +0000

    sort.h

--- sort.h
+++ sort.h
@@ -8,5 +8,6 @@ void bubble_sort(int *array, size_t size);
 
 /* Helper function */
 void print_array(const int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
 #endif /* SORT_H */
